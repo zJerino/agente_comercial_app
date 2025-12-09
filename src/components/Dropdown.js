77d5a7ee60d6c599b1f9c20bf4040311f0d31569
zJerino <zJerino@gmail.com>
2025-12-09 16:44:29 -0400

    Se agrego el atributo key para solucionar la advertencia de react

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -11,18 +11,18 @@ export const itemsScheme = {
 };
 
 export default function Dropdown({ items = [], title = <i className="bi bi-three-dots"></i>, btnclass = 'btn btn-solid-primary my-2', className = ""}) {
-    items = items.map((item) => {
+    items = items.map((item, index) => {
         if (typeof item == 'object') {
             item = Object.assign({...itemsScheme}, item);
             if (item.modal.length > 0) {
                 return (
-                    <label className="dropdown-item text-sm flex-row gap-1" htmlFor={item.modal}>
+                    <label key={index} className="dropdown-item text-sm flex-row gap-1" htmlFor={item.modal}>
                         {item.icon.length > 0 ? <i className={'bi bi-' + item.icon}></i> : ''} <span>{item.text}</span>
                     </label>
                 );
             } else {
                 return (
-                    <Link className="dropdown-item text-sm flex-row gap-1">
+                    <Link key={index} className="dropdown-item text-sm flex-row gap-1">
                         {item.icon.length > 0 ? <i className={'bi bi-' + item.icon}></i> : ''} <span>{item.text}</span>
                     </Link>
                 );
