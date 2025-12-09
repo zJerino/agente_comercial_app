d95220d6d90a25bcaef2f429353dbd7844608e9d
zJerino <zJerino@gmail.com>
2025-12-09 16:32:18 -0400

    Se añadio un componente para ver el precio del dolar en venezuela

---
+++ src/components/DolarPrice.js
@@ -0,0 +1,114 @@
+import { useEffect, useState } from 'react';
+import { useLocalStorage, useCopyToClipboard } from 'react-use';
+import { tryParseJSON } from '../configs/utils';
+
+/**
+ * Esquema de respuesta de la API
+ */
+const apiRespondeSchema = {
+    fuente: "",
+    nombre: "",
+    compra: 0,
+    venta: 0,
+    promedio: 0.00,
+    fechaActualizacion: ""
+}
+
+/**
+ * Nos ayuda a saber si la fecha todavia esta bien
+ */
+function isUpdated(dateStringOrObject) {
+    const fechaOriginal = new Date(dateStringOrObject);
+
+    if (isNaN(fechaOriginal.getTime())) {
+        console.error("Error: La entrada no es una fecha válida.");
+        return false;
+    }
+    const fechaExpiracion = new Date(fechaOriginal);
+
+    fechaExpiracion.setDate(fechaExpiracion.getDate() + 1);
+    fechaExpiracion.setHours(18, 0, 0, 0);
+
+    const fechaActual = new Date();
+    return fechaActual.getTime() <= fechaExpiracion.getTime();
+}
+
+/**
+ * Esto es un componente que indica el precio del dolar (Venezuela)
+ */
+export default function Main() {
+    const [loading, setLoading] = useState(true);
+    // eslint-disable-next-line
+    const [error, setError] = useState(null);
+    // eslint-disable-next-line
+    const [state, copyToClipboard] = useCopyToClipboard();
+    const [value, setValue] = useLocalStorage('dolarbcv', {});
+
+    let data = tryParseJSON(value) || {};
+    data = Object.assign({...apiRespondeSchema}, data);
+
+    let status;
+
+    useEffect(() => {
+        const fetchData = async () => {
+          try {
+            let api = await fetch('https://ve.dolarapi.com/v1/dolares/oficial');              
+            
+            if (!api.ok) {
+              throw new Error(`HTTP error! status: ${api.status}`);
+            }
+
+            const json = await api.json();
+            setValue(JSON.stringify(Object.assign({...apiRespondeSchema}, json)));
+          } catch (err) {
+            setError(err);
+          } finally {
+            setLoading(false);
+          }
+        };
+
+        if (data.fechaActualizacion.length <= 0 || !isUpdated(data.fechaActualizacion)) {
+            fetchData();
+        } else {
+            setLoading(false);
+        }
+    }, [data.fechaActualizacion, setValue]);
+
+    /**
+     * Si no existe fecha
+     */
+    if (data.fechaActualizacion.length > 0) {
+        const lastupdate = new Date(data.fechaActualizacion);
+        status = `Ultima actualizacion: ${lastupdate.toLocaleDateString()} ${lastupdate.getHours()}:${lastupdate.getMinutes()}`;
+    }
+    
+    if (loading) {
+        return (
+            <div className="bg-green-600 rounded-[1rem] px-4 py-3 m-2 text-white flex items-center gap-5">
+                <i className="bi bi-currency-dollar text-[2.5rem]"></i>
+                <div className="flex flex-col grow-[1]">
+                    <div className="flex flex-row">
+                        <span className="font-[700] fv-small">Dolar BCV</span>
+                        <a className="ms-auto" href="https://www.bcv.org.ve/#:~:text=USD" rel="noreferrer" target="_blank"><i className="bi bi-box-arrow-up-right px-2 text-white text-decoration-none"></i></a>
+                    </div>
+                    <h3 className="text-[1.25rem] font-[500]"><span className="flex skeleton-pulse w-full min-h-[1.35rem] my-auto"></span></h3>
+                </div>
+            </div>
+        );
+    }
+
+    return (
+        <div className="bg-green-600 rounded-[1rem] px-4 py-3 m-2 text-white flex items-center gap-5">
+            <i className="bi bi-currency-dollar text-[2.5rem]"></i>
+            <div className="flex flex-col grow-[1]">
+                <div className="flex flex-row leading-[.85rem]">
+                    <span className="font-[700] fv-small">Dolar BCV</span>
+                    {/* Pronto: agregar aqui un enlace para abrir una calculadora con el precio del dolar <i className="ms-auto bi bi-calculator-fill px-2"></i> */}
+                    <a className="ms-auto" href="https://www.bcv.org.ve/#:~:text=USD" rel="noreferrer" target="_blank"><i className="bi bi-box-arrow-up-right px-2 text-white text-decoration-none"></i></a>
+                </div>
+                <h3 className="text-[1.25rem] font-[500]"><span onClick={() => copyToClipboard(data.promedio)}>{data.promedio}</span> Bs</h3>
+                <h5 className="text-[0.9rem] font-[200]">{status}</h5>
+            </div>
+        </div>
+    );
+}
--- src/routes/home.js
+++ src/routes/home.js
@@ -1,11 +1,10 @@
+import DolarPrice from '../components/DolarPrice';
 
 export default function Home() {
+
     return (
-        <>
-            <div className="bg-primary rounded-[1rem] px-3 py-2 m-2 text-white flex flex-col">
-                <h3 className="text-[1.25rem] font-[500]">Welcome</h3>
-                <h5 className="text-[0.9rem] font-[200]">Hoy el dolar amanecio bien caro</h5>
-            </div>
-        </>
+        <div className="flex flex-col gap-1">
+            <DolarPrice />
+        </div>
     );
 }
