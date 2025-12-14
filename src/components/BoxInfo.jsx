0928273a827786bdebef830133f8e9406578dec1
zJerino <zJerino@gmail.com>
2025-12-13 22:25:38 -0400

    Solucion al error de escritura de className en boxinfo

--- src/components/BoxInfo.jsx
+++ src/components/BoxInfo.jsx
@@ -1,15 +1,15 @@
 export default function Main({ bgColor = 'gray-200', textColor = 'black', icon = 'box-seam-fill', title = "", text = ''}) {
     return (
         <div className={"bg-" + bgColor + " rounded-[1rem] px-4 py-3 text-"+ textColor +" flex items-center gap-2"}>
-            <i class={"bi bi-"+icon+" text-[2.5rem]"}></i>
-            <div class="flex flex-col grow-[1]">
+            <i className={"bi bi-"+icon+" text-[2.5rem]"}></i>
+            <div className="flex flex-col grow-[1]">
                 {title.length > 0 ? (
-                    <div class="flex flex-row leading-[.85rem]">
-                        <span class="font-[700] fv-small">{title}</span>
+                    <div className="flex flex-row leading-[.85rem]">
+                        <span className="font-[700] fv-small">{title}</span>
                     </div>
                 ) : null}
                 {String(text).length > 0 ? (
-                    <h3 class="text-[1.25rem] font-[500]">
+                    <h3 className="text-[1.25rem] font-[500]">
                         <span>{String(text)}</span>
                     </h3>
                 ) : null}
